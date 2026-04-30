add4cb76670c2d6ec1973b9a6f5fc6eb21731a2c
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-04-30 19:48:04 -0300

    feat: Adapter

--- src/adapters/showMessage.ts
+++ src/adapters/showMessage.ts
@@ -1,10 +1,24 @@
 import { toast } from "react-toastify";
-
+import { Dialog } from "../components/Dialog";
+import { createElement } from "react";
 export const showMessage = {
-    success: (msg: string) => toast.success(msg),
-    error: (msg: string) =>  toast.error(msg),
-    warn: (msg: string) =>  toast.warn(msg),
-    warning: (msg: string) =>  toast.warning(msg),
-    info: (msg: string) =>  toast.info(msg),
-    dismiss: () => toast.dismiss()
-}
+  success: (msg: string) => toast.success(msg),
+  error: (msg: string) => toast.error(msg),
+  warn: (msg: string) => toast.warn(msg),
+  warning: (msg: string) => toast.warning(msg),
+  info: (msg: string) => toast.info(msg),
+  dismiss: () => toast.dismiss(),
+  confirm: (data: string, onClosing: (confirmation: boolean) => void) =>
+    toast((props) => createElement(Dialog, props), {
+      data,
+      onClose: (confirmation) => {
+        if (confirmation) return onClosing(true);
+        return onClosing(false);
+      },
+      autoClose: false,
+      closeOnClick: false,
+      closeButton: false,
+      draggable: false,
+      position: 'top-center'
+    }),
+};
